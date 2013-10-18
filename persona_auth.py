4c3fff5a51526a11dcb7a3c1b7951ebe7cac7864
Tarek Ziade <tarek@ziade.org>
2013-10-18 16:07:49 +0200

    allow non-persona log ins for now

--- persona_auth.py
+++ persona_auth.py
@@ -19,8 +19,6 @@ def read_stdin():
     """
     # get the record size in the first two bytes
     size, = unpack('>h', sys.stdin.read(2))
-
-
     # read the record
     return sys.stdin.read(size).split(':')
 
@@ -35,9 +33,14 @@ def return_result(result=True):
 def auth(user, host, password):
     """Authenticates a user.
     """
+    if password.startswith('persona:'):
+        password = password[len('persona:'):]
+    else:
+        # just accept any password for other people for now
+        return True
     # the password is the assertion
     try:
-        verifier.verify(password, host)
+        verifier.verify(password, '*')
     except ValueError, UnicodeDecodeError:
         return False
 
