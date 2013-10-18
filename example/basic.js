3751fd814c3e0a6acdbc81f4acae651cafc712a7
Tarek Ziade <tarek@ziade.org>
2013-10-18 16:08:20 +0200

    plugged persona and strophe.js

--- example/basic.js
+++ example/basic.js
@@ -1,7 +1,10 @@
-var BOSH_SERVICE = 'http://localhost:5280/xmpp-httpbind'
+//var BOSH_SERVICE = 'http://localhost:5280/xmpp-httpbind'
+var BOSH_SERVICE = 'http://localhost:5280/http-bind'
+
+
 var connection = null;
 
-function log(msg) 
+function log(msg)
 {
     $('#log').append('<div></div>').append(document.createTextNode(msg));
 }
@@ -16,22 +19,39 @@ function rawOutput(data)
     log('SENT: ' + data);
 }
 
+function onMessage(msg) {
+    var to = msg.getAttribute('to');
+    var from = msg.getAttribute('from');
+    var type = msg.getAttribute('type');
+    var elems = msg.getElementsByTagName('body');
+
+    if (type == "chat" && elems.length > 0) {
+      var body = elems[0];
+      alert(Strophe.getText(body));
+    }
+    return true;
+}
+
 function onConnect(status)
 {
     if (status == Strophe.Status.CONNECTING) {
 	log('Strophe is connecting.');
     } else if (status == Strophe.Status.CONNFAIL) {
 	log('Strophe failed to connect.');
-	$('#connect').get(0).value = 'connect';
     } else if (status == Strophe.Status.DISCONNECTING) {
 	log('Strophe is disconnecting.');
     } else if (status == Strophe.Status.DISCONNECTED) {
 	log('Strophe is disconnected.');
-	$('#connect').get(0).value = 'connect';
     } else if (status == Strophe.Status.CONNECTED) {
 	log('Strophe is connected.');
-	connection.disconnect();
+  // hack the domain for bow 
+  var from = currentUser.split('@')[0] + '@localhost';
+  connection.addHandler(onMessage, null, 'message', null, null,  null);
+  connection.send($msg({to: 'tarek2@localhost', from: from, 
+     type: 'chat'}).c("body").t('some data'));
+
     }
+
 }
 
 $(document).ready(function () {
@@ -39,17 +59,4 @@ $(document).ready(function () {
     connection.rawInput = rawInput;
     connection.rawOutput = rawOutput;
 
-    $('#connect').bind('click', function () {
-	var button = $('#connect').get(0);
-	if (button.value == 'connect') {
-	    button.value = 'disconnect';
-
-	    connection.connect($('#jid').get(0).value,
-			       $('#pass').get(0).value,
-			       onConnect);
-	} else {
-	    button.value = 'connect';
-	    connection.disconnect();
-	}
-    });
 });
--- server.py
+++ server.py
@@ -1,12 +1,64 @@
 import os
-from bottle import route, run, template, static_file
+import bottle
+from bottle import (route, run, template, static_file, redirect,
+                    request, template)
+import browserid
+import beaker.middleware
+
+session_opts = {
+    'session.type': 'file',
+    'session.data_dir': './session/',
+    'session.auto': True,
+    }
 
 STATIC = os.path.abspath(os.path.join(os.path.dirname(__file__), 'example'))
 
+verifier = browserid.LocalVerifier(['*'])
+app = beaker.middleware.SessionMiddleware(bottle.app(), session_opts)
+
 
 @route('/<filename:path>')
-def index(filename):
+def static(filename):
     return static_file(filename, root=STATIC)
 
 
-run(host='localhost', port=8080)
+@route('/basic.html')
+def index():
+    with open(os.path.join(STATIC, 'basic.html')) as f:
+        data = f.read()
+
+    session = request.environ.get('beaker.session')
+    return template(data, session=session)
+
+
+@route('/login', method='POST')
+def login():
+    assertion = request.POST['assertion']
+    try:
+        data = verifier.verify(assertion, '*')
+        email = data['email']
+        app_session = request.environ.get('beaker.session')
+        app_session['logged_in'] = True
+        app_session['email'] = email
+        app_session['assertion'] = assertion
+    except ValueError, UnicodeDecodeError:
+        # need to raise a auth
+        pass
+    return {'email': email}
+
+
+@route('/logout', method='POST')
+def logout():
+    app_session = request.environ.get('beaker.session')
+    app_session['logged_in'] = False
+    app_session['email'] = None
+    redirect("/basic.html")
+
+
+@route('/')
+def _index():
+    redirect("/basic.html")
+
+
+
+run(host='localhost', port=8080, app=app)
